9e7c8677dfb73931495f25c748e46bc37c3f2608
Santiago Parini <santiago.parini@globallogic.com>
2017-11-17 14:46:44 -0300

    isIPv4Address

---
+++ arcade/Island of knowledge/isIPv4Address.php
@@ -0,0 +1,19 @@
+/*An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+IPv4 addresses are represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255 inclusive, separated by dots, e.g., 172.16.254.1.
+Given a string, find out if it satisfies the IPv4 address naming rules.*/
+
+function isIPv4Address($inputString)
+{
+    if(preg_match("/[a-z]/i", $inputString)) return false;
+    
+    $ipv4 = explode('.',$inputString);
+    if(sizeOf($ipv4) != 4) return false;
+   
+    foreach($ipv4 as $pivot)
+    {
+        if ($pivot == null) return false;
+        if ($pivot < 0 || $pivot > 255) return false;
+    }   
+    
+    return true;
+}
