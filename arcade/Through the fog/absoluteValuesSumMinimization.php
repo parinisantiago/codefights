4c5139b51b165628e38ad85afefd6dab1479e029
Santiago Parini <santiago.parini@globallogic.com>
2018-01-04 17:07:07 -0300

    absoluteValuesSumMinimization

---
+++ arcade/Through the fog/absoluteValuesSumMinimization.php
@@ -0,0 +1,11 @@
+/*Given a sorted array of integers a, find an integer x from a such that the value of
+
+abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+is the smallest possible (here abs denotes the absolute value).
+If there are several possible answers, output the smallest one.
+
+*/
+
+function absoluteValuesSumMinimization($a) {
+    return $a[ceil(sizeOf($a)/2)-1];
+}
